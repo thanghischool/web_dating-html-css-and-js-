cd5c0332bcc08237c0069a38a520dffeb0654b75
Nguyễn Thiện Thắng <thangxpro999@gmail.com>
2023-07-08 20:18:22 +0700

    Them chuc nang

--- web_dating/js/searching/searching.js
+++ web_dating/js/searching/searching.js
@@ -1,141 +1,150 @@
 var data = [
-        [{
-            id: 1,
-            name: 'Du lịch',
-            icon: 'img/searching/dulich.png',
-            checked: false
-        },
-        {
-            id: 2,
-            name: 'Học tập',
-            icon: 'img/searching/hoctap.png',
-            checked: false
-        },
-        {
-            id: 3,
-            name: 'Thức ăn',
-            icon: 'img/searching/thucan.png',
-            checked: false
-        },
-        {
-            id: 4,
-            name: 'Tình cảm',
-            icon: 'img/searching/tinhcam.png',
-            checked: false
-        },
-        {
-            id: 5,
-            name: 'Nghệ thuật',
-            icon: 'img/searching/nghethuat.png',
-            checked: false
-        },
-        {
-            id: 6,
-            name: 'Công nghệ',
-            icon: 'img/searching/congnghe.png',
-            checked: false
-        },
-        {
-            id: 7,
-            name: 'Làm đẹp',
-            icon: 'img/searching/lamdep.png',
-            checked: false
-        },
-        {
-            id: 8,
-            name: 'Sáng tạo',
-            icon: 'img/searching/sangtao.png',
-            checked: false
-        },
-        {
-            id: 9,
-            name: 'Trò chơi',
-            icon: 'img/searching/trochoi.png',
-            checked: false
-        },
-        {
-            id: 10,
-            name: 'Kinh nghiệm',
-            icon: 'img/searching/kinhnghiem.png',
-            checked: false
-        },
-        {
-            id: 11,
-            name: 'Công việc',
-            icon: 'img/searching/congviec.png',
-            checked: false
-        },
-        {
-            id: 12,
-            name: 'Điện ảnh',
-            icon: 'img/searching/dienanh.png',
-            checked: false
-        },
-        {
-            id: 13,
-            name: 'Sách',
-            icon: 'img/searching/sach.png',
-            checked: false
-        },
-        {
-            id: 14,
-            name: 'Âm nhạc',
-            icon: 'img/searching/amnhac.png',
-            checked: false
-        },
-        {
-            id: 15,
-            name: 'Thể thao',
-            icon: 'img/searching/thethao.png',
-            checked: false
-        }
-    ],
-    [
-        {
-            id: 1,
-            name: "Nam",
-            icon: 'img/searching/male.png',
-            checked: false
-        },
-        {
-            id: 2,
-            name: "Nữ",
-            icon: 'img/searching/female.png',
-            checked: false
-        },
-        {
-            id: 3,
-            name: "LGBT",
-            icon: 'img/searching/LGBT.png',
-            checked: false
-        }
-    ]
+  [
+    {
+      id: 1,
+      name: "Du lịch",
+      icon: "img/searching/dulich.png",
+      checked: false,
+    },
+    {
+      id: 2,
+      name: "Học tập",
+      icon: "img/searching/hoctap.png",
+      checked: false,
+    },
+    {
+      id: 3,
+      name: "Thức ăn",
+      icon: "img/searching/thucan.png",
+      checked: false,
+    },
+    {
+      id: 4,
+      name: "Tình cảm",
+      icon: "img/searching/tinhcam.png",
+      checked: false,
+    },
+    {
+      id: 5,
+      name: "Nghệ thuật",
+      icon: "img/searching/nghethuat.png",
+      checked: false,
+    },
+    {
+      id: 6,
+      name: "Công nghệ",
+      icon: "img/searching/congnghe.png",
+      checked: false,
+    },
+    {
+      id: 7,
+      name: "Làm đẹp",
+      icon: "img/searching/lamdep.png",
+      checked: false,
+    },
+    {
+      id: 8,
+      name: "Sáng tạo",
+      icon: "img/searching/sangtao.png",
+      checked: false,
+    },
+    {
+      id: 9,
+      name: "Trò chơi",
+      icon: "img/searching/trochoi.png",
+      checked: false,
+    },
+    {
+      id: 10,
+      name: "Kinh nghiệm",
+      icon: "img/searching/kinhnghiem.png",
+      checked: false,
+    },
+    {
+      id: 11,
+      name: "Công việc",
+      icon: "img/searching/congviec.png",
+      checked: false,
+    },
+    {
+      id: 12,
+      name: "Điện ảnh",
+      icon: "img/searching/dienanh.png",
+      checked: false,
+    },
+    {
+      id: 13,
+      name: "Sách",
+      icon: "img/searching/sach.png",
+      checked: false,
+    },
+    {
+      id: 14,
+      name: "Âm nhạc",
+      icon: "img/searching/amnhac.png",
+      checked: false,
+    },
+    {
+      id: 15,
+      name: "Thể thao",
+      icon: "img/searching/thethao.png",
+      checked: false,
+    },
+  ],
+  [
+    {
+      id: 1,
+      name: "Nam",
+      icon: "img/searching/male.png",
+      checked: false,
+    },
+    {
+      id: 2,
+      name: "Nữ",
+      icon: "img/searching/female.png",
+      checked: false,
+    },
+    {
+      id: 3,
+      name: "LGBT",
+      icon: "img/searching/LGBT.png",
+      checked: false,
+    },
+  ],
 ];
-function resetChecked(){
-    for (var i = 0; i < data.length; i++) {
-        data[i].forEach(function(value){
-            value.checked = false;
-        });
-    };
+
+var otherUserApi = "http://localhost:3000/otherUser";
+function resetChecked() {
+  for (var i = 0; i < data.length; i++) {
+    data[i].forEach(function (value) {
+      value.checked = false;
+    });
+  }
 }
 /////////////////////////////////////////////
 var valueRange = document.getElementById("value-range");
-        var range = document.getElementById("range");
-        range.addEventListener("input",function(e) {
-            valueRange.innerHTML = range.value;
-        });
-        range.addEventListener("change",function(e) {
-            valueRange.innerHTML = range.value;
-        });
-var thrumb = document.querySelector(".range-form #range");
-thrumb.addEventListener("input",function(e) {
-    var v = (thrumb.clientWidth/(thrumb.max-thrumb.min));
-    thrumb.style.background = `linear-gradient(90deg, #ED147D ${v.toFixed(6)*(thrumb.value-thrumb.min)+10*((thrumb.max-thrumb.value)-(thrumb.value-thrumb.min))/34}px, rgb(214,214,214) 0%)`;
+var range = document.getElementById("range");
+range.addEventListener("input", function (e) {
+  valueRange.innerHTML = range.value;
 });
-window.onresize = function (){
-    var v = (thrumb.clientWidth/(thrumb.max-thrumb.min));
-    thrumb.style.background = `linear-gradient(90deg, #ED147D ${v.toFixed(6)*(thrumb.value-thrumb.min)+10*((thrumb.max-thrumb.value)-(thrumb.value-thrumb.min))/34}px, rgb(214,214,214) 0%)`;
-}
+range.addEventListener("change", function (e) {
+  valueRange.innerHTML = range.value;
+});
+var thrumb = document.querySelector(".range-form #range");
+thrumb.addEventListener("input", function (e) {
+  var v = thrumb.clientWidth / (thrumb.max - thrumb.min);
+  thrumb.style.background = `linear-gradient(90deg, #ED147D ${
+    v.toFixed(6) * (thrumb.value - thrumb.min) +
+    (10 * (thrumb.max - thrumb.value - (thrumb.value - thrumb.min))) / 34
+  }px, rgb(214,214,214) 0%)`;
+});
+window.onresize = function () {
+  var v = thrumb.clientWidth / (thrumb.max - thrumb.min);
+  thrumb.style.background = `linear-gradient(90deg, #ED147D ${
+    v.toFixed(6) * (thrumb.value - thrumb.min) +
+    (10 * (thrumb.max - thrumb.value - (thrumb.value - thrumb.min))) / 34
+  }px, rgb(214,214,214) 0%)`;
+};
 //////////////////////////////////////////////////////////////////
 var up1 = document.querySelector(".topic span i");
 var up2 = document.querySelector(".age span i");
@@ -145,128 +154,193 @@ var box2 = document.querySelector(".range-form");
 var box3 = document.querySelector(".gender-form");
 var topic = document.querySelector(".topic");
 var gender = document.querySelector(".gender");
-var age = document.querySelector('.age');
+var age = document.querySelector(".age");
 var input = document.querySelector('input[type="text"]');
 var cancel = document.querySelector(".cancel-btn");
-var searchbtn = document.querySelector(".search-button")
+var searchbtn = document.querySelector(".search-button");
 /////////////////////////////
-function showRange(){
-    box2.style.display = "block";
+function showRange() {
+  box2.style.display = "block";
 }
-age.addEventListener('click',function(e){
-    box3.style.display = "none";
-    up3.style.transform = "rotate(0deg)";
-    box1.style.display = "none";
-    up1.style.transform = "rotate(0deg)";
-    if (box2.style.display == "none"){
-        showRange();
-        up2.style.transform = "rotate(180deg)";
-    } else {
-        box2.style.display = "none";
-        up2.style.transform = "rotate(0deg)";
-    }
+age.addEventListener("click", function (e) {
+  box3.style.display = "none";
+  up3.style.transform = "rotate(0deg)";
+  box1.style.display = "none";
+  up1.style.transform = "rotate(0deg)";
+  if (box2.style.display == "none") {
+    showRange();
+    up2.style.transform = "rotate(180deg)";
+  } else {
+    box2.style.display = "none";
+    up2.style.transform = "rotate(0deg)";
+  }
 });
 ////////////////////////////
-function showGender(){
-    box3.style.display = "inline-flex";
+function showGender() {
+  box3.style.display = "inline-flex";
 }
-gender.addEventListener("click",function(e){
-    box2.style.display = "none";
-    up2.style.transform = "rotate(0deg)";
-    box1.style.display = "none";
-    up1.style.transform = "rotate(0deg)";
-    if (box3.style.display == "none"){
-        showGender();
-        up3.style.transform = "rotate(180deg)";
-    } else {
-        box3.style.display = "none";
-        up3.style.transform = "rotate(0deg)";
-        }
+gender.addEventListener("click", function (e) {
+  box2.style.display = "none";
+  up2.style.transform = "rotate(0deg)";
+  box1.style.display = "none";
+  up1.style.transform = "rotate(0deg)";
+  if (box3.style.display == "none") {
+    showGender();
+    up3.style.transform = "rotate(180deg)";
+  } else {
+    box3.style.display = "none";
+    up3.style.transform = "rotate(0deg)";
+  }
 });
 var items = document.getElementsByClassName("item");
-function choosed(value){
-    if (items[value].style.backgroundColor == "rgba(237, 20, 125, 0.8)"){
-        items[value].style.backgroundColor = "#FFF";
-    } else items[value].style.backgroundColor = "rgba(237, 20, 125, 0.8)";
+function choosed(value) {
+  if (items[value].style.backgroundColor == "rgba(237, 20, 125, 0.8)") {
+    items[value].style.backgroundColor = "#FFF";
+  } else items[value].style.backgroundColor = "rgba(237, 20, 125, 0.8)";
 }
 ///////////////////////////////////////
-function showTopic(){
-    box1.style.display = "inline-flex";
+function showTopic() {
+  box1.style.display = "inline-flex";
 }
-topic.addEventListener("click",function(e){
-    box3.style.display = "none";
-    up3.style.transform = "rotate(0deg)";
-    box2.style.display = "none";
-    up2.style.transform = "rotate(0deg)";
-    if (box1.style.display == "none"){
-        showTopic();
-        up1.style.transform = "rotate(180deg)";
-    } else {
-        box1.style.display = "none";
-        up1.style.transform = "rotate(0deg)";
-        }
-});
-function loadData(){
-    var content = data[0].map(function(value){
-        return `<div class="item1" style="background: #FFF" data_id="${value.id}" onclick="checked(${value.id-1},0,'item1')">
-        <img src="${value.icon}">
-        ${value.name}
-    </div>`;
-    }).join("");
-    var topicForm = document.querySelector(".topic-form");
-    topicForm.innerHTML = content;
-    content = data[1].map(function(value){
-        return `<div class="item2" style="background: #FFF" data_id="${value.id}" onclick="checked(${value.id-1},1,'item2')">
-        <img src="${value.icon}">
-        ${value.name}
-    </div>`;
-    }).join("");
-    var genderForm = document.querySelector(".gender-form");
-    genderForm.innerHTML = content;
-}
-loadData();
-function checked(value,key,item){
-    var items = document.getElementsByClassName(item);
-    if (!(data[key][value].checked)){
-        items[value].style.backgroundColor = "rgba(237, 20, 125, 0.8)";
-        data[key][value].checked = true;
-    } else {
-        items[value].style.backgroundColor = "#FFF";
-        data[key][value].checked = false;
-    }
-}
-///////////////////////////////////////
-topic.addEventListener("click",function(){
-    input.style.display = "none";
-    cancel.style.display = "flex";
-    searchbtn.style.display = "flex";
-});
-gender.addEventListener("click",function(){
-    input.style.display = "none";
-    cancel.style.display = "flex";
-    searchbtn.style.display = "flex";
-});
-age.addEventListener("click",function(){
-    input.style.display = "none";
-    cancel.style.display = "flex";
-    searchbtn.style.display = "flex";
-});
-///////////////////////////////////////
-function clear(){
-    resetChecked();
-    loadData();
-    range.value = 16;
-    thrumb.style.background = `linear-gradient(90deg, #ED147D 0px, rgb(214,214,214) 0%)`;
-    input.value="";
-    input.style.display = "flex";
-    cancel.style.display = "none";
-    searchbtn.style.display = "none";
-    box3.style.display = "none";
-    up3.style.transform = "rotate(0deg)";
-    box2.style.display = "none";
-    up2.style.transform = "rotate(0deg)";
+topic.addEventListener("click", function (e) {
+  box3.style.display = "none";
+  up3.style.transform = "rotate(0deg)";
+  box2.style.display = "none";
+  up2.style.transform = "rotate(0deg)";
+  if (box1.style.display == "none") {
+    showTopic();
+    up1.style.transform = "rotate(180deg)";
+  } else {
     box1.style.display = "none";
     up1.style.transform = "rotate(0deg)";
-
+  }
+});
+function loadData() {
+  var content = data[0]
+    .map(function (value) {
+      return `<div class="item1" style="background: #FFF" data_id="${
+        value.id
+      }" onclick="checked(${value.id - 1},0,'item1')">
+        <img src="${value.icon}">
+        ${value.name}
+    </div>`;
+    })
+    .join("");
+  var topicForm = document.querySelector(".topic-form");
+  topicForm.innerHTML = content;
+  content = data[1]
+    .map(function (value) {
+      return `<div class="item2" style="background: #FFF" data_id="${
+        value.id
+      }" onclick="checked(${value.id - 1},1,'item2')">
+        <img src="${value.icon}">
+        ${value.name}
+    </div>`;
+    })
+    .join("");
+  var genderForm = document.querySelector(".gender-form");
+  genderForm.innerHTML = content;
+}
+loadData();
+function checked(value, key, item) {
+  var items = document.getElementsByClassName(item);
+  if (!data[key][value].checked) {
+    items[value].style.backgroundColor = "rgba(237, 20, 125, 0.8)";
+    data[key][value].checked = true;
+  } else {
+    items[value].style.backgroundColor = "#FFF";
+    data[key][value].checked = false;
+  }
+}
+///////////////////////////////////////
+topic.addEventListener("click", function () {
+  input.style.display = "none";
+  cancel.style.display = "flex";
+  searchbtn.style.display = "flex";
+});
+gender.addEventListener("click", function () {
+  input.style.display = "none";
+  cancel.style.display = "flex";
+  searchbtn.style.display = "flex";
+});
+age.addEventListener("click", function () {
+  input.style.display = "none";
+  cancel.style.display = "flex";
+  searchbtn.style.display = "flex";
+});
+///////////////////////////////////////
+function clear() {
+  resetChecked();
+  loadData();
+  range.value = 16;
+  thrumb.style.background = `linear-gradient(90deg, #ED147D 0px, rgb(214,214,214) 0%)`;
+  input.value = "";
+  input.style.display = "flex";
+  cancel.style.display = "none";
+  searchbtn.style.display = "none";
+  box3.style.display = "none";
+  up3.style.transform = "rotate(0deg)";
+  box2.style.display = "none";
+  up2.style.transform = "rotate(0deg)";
+  box1.style.display = "none";
+  up1.style.transform = "rotate(0deg)";
 }
 cancel.addEventListener("click", clear);
+
+fetch(otherUserApi)
+  .then(function (response) {
+    return response.json();
+  })
+
+  .then(function (data) {
+    let inputUser = document.querySelector(".input #search-input");
+    let showUser = document.querySelector(".showUsers");
+    let search = document.querySelector(".search-button");
+    let inputValue;
+    inputUser.oninput = function (e) {
+      inputValue = e.target.value;
+      let userSearch = data.filter(function (user) {
+        return user.name.toLowerCase() == inputValue.toLowerCase();
+      });
+
+      search.addEventListener("click", function () {
+        if (userSearch.length !== 0) {
+          for (let i in userSearch) {
+            showUser.innerHTML = `<div class="sub-showUser" style = "width: 524px; border-radius: 20px; text-align: center; box-shadow: 4px 4px 38px 0px rgba(0, 0, 0, 0.25); margin-left: 10%; padding-bottom: 2%">
+              <img class="card-img-top" src="${userSearch[i].img_avatar}" alt="Ảnh user" style="border-radius: 20px 20px">
+              <p style = "font-size: 60px">${userSearch[i].name}</p>
+              <p style = "font-size: 40px">${userSearch[i].Yearofbirth}</p>
+              <div style = "display: inline-block; margin: 0 auto"> 
+              <p style="width: 127px; height: 37px; display: inline-block; border: 1px solid #9747FF; border-radius: 50px">${userSearch[i].Hoobies[0]}</p>
+              <p style="width: 127px; height: 37px; display: inline-block; border: 1px solid #9747FF; border-radius: 50px">${userSearch[i].Hoobies[1]}</p>
+              </div>
+
+
+              <p style="width: 127px; height: 37px; margin-left: 50%; translate: -50% 0 0; border: 1px solid #9747FF; border-radius: 50px">${userSearch[i].Hoobies[2]}</p>
+              
+              </div>`;
+          }
+        } else {
+          showUser.innerHTML = `<div class="message" style="text-align: center"><h1>Không tìm thấy dữ liệu</h1></div>`;
+        }
+      });
+    };
+  })
+
+  .catch(function (err) {
+    console.log(err);
+    alert("Lỗi dữ liệu");
+  });
+
+cancel.onclick = function () {
+  let messageElement = document.querySelector(".message");
+  let subShowUserElement = document.querySelector(".sub-showUser");
+
+  if (messageElement) {
+    messageElement.remove();
+  }
+
+  if (subShowUserElement) {
+    subShowUserElement.remove();
+  }
+};
